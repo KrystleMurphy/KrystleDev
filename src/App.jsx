f3e3e8b934e8b0ee7501dfb45f8c4d51276916e1
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-07-26 17:19:09 +0100

    new portfolio July 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,30 @@
 import React from "react";
-import bgImage from "../public/bgImage.jpg";
-import CV from "../public/CV.pdf";
+import Navbar from "./Components/Navbar";
+import Hero from "./Hero";
+import Why from "./Why"
+import CTA from "./CTA"
+import Testimonials from "./Testemonials";
+import Projects from "./Projects";
+import ProjectsTwo from "./ProjectsTwo";
+import Contact from "./Components/Contact"
+import WholePage from "./WholePage"
 
-export default function ComingSoon() {
+export default function App() {
   return (
-    <div
-      className="min-h-screen flex items-center justify-center px-6 sm:px-8 bg-cover"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <div className="max-w-2xl text-center text-white bg-slate-900 rounded-lg bg-opacity-90 p-20">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Krystle.Dev</h1>
-        <div className="mt-6 text-lg">
-          <h2 className="font-bold">🎉 Exciting News Ahead! 🎉</h2>
-          <p className="mt-4">
-            I'm currently crafting something extraordinary for you! Stay tuned as we
-            gear up to unveil a stunning new showcase portfolio. From sleek designs to seamless interactions, get ready
-            to explore the artistry of modern web development firsthand. Keep
-            checking back, I can't wait to share it with you!
-          </p>
-          <p className="mt-4">
-            If you can't wait, you can 
-            <a 
-              href="https://reliable-arithmetic-010ab5.netlify.app" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 p-2 m-2 inline-block"
-            >
-              view an old portfolio
-            </a>
-            However, I've come a long way since then!
-          </p>
-          <p>
-          <a 
-  href={CV} 
-  target="_blank"
-  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 p-2 m-2 inline-block"
->
-  View CV
-</a>
-<br></br>
-<a 
-  href={CV} 
-  download 
-  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 p-2 m-2 inline-block"
->
-  Download a copy of Krystle's CV
-</a>
+    <>
+      <Navbar />
 
-       
-          </p>
-        </div>
-      </div>
-    </div>
+
+
+    {/* <WholePage /> */}
+      {/* <Hero /> */}
+      {/* <Why /> */}
+      {/* <Testimonials /> */}
+      {/* <CTA /> */}
+      {/* <Projects /> */}
+      {/* <ProjectsTwo /> */}
+      <Contact />
+
+      </>
   );
 }
-
---
+++ src/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <p className="text-base font-semibold leading-7 text-indigo-600">Krystle.Dev</p>
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Front-End Developer | Innovator in Web Solutions</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+         Languages include JavaScript, CSS, HTML alongside experience utilising frameworks like Bootstrap & Tailwind. I specialize in creating seamless and engaging user interfaces, integrating third-party APIs and managing projects with NodeJS and Git. I love solving complex problems and deliver quality, scalable web solutions using the latest technologies.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
