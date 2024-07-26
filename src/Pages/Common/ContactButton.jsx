ca6237bb7ea7e386fafda86ea99bdb8b56303cc8
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-07-26 18:12:40 +0100

    fix: contact button

--- src/Pages/Common/ContactButton.jsx
+++ src/Pages/Common/ContactButton.jsx
@@ -1,15 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ContactButton() {
     return (
-<>
-<div className="mt-10 flex">
-<a
-  href="/contact"
-  className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-slate-50 shadow-sm hover:bg-cyan-500 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
->
-  Contact <span aria-hidden="true">&rarr;</span>
-</a>
-</div>
-</>
+        <>
+            <div className="mt-10 flex">
+                <Link
+                    to="/contact"
+                    className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-slate-50 shadow-sm hover:bg-cyan-500 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                >
+                    Contact <span aria-hidden="true">&rarr;</span>
+                </Link>
+            </div>
+        </>
     );
-  }
+}
